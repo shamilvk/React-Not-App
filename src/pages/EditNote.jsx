22d9adb2a8aa61514d8c1840379bc66f16e10163
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-02-01 02:03:58 +0530

    Edit Note functionality first commit

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,9 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { IoIosArrowBack } from 'react-icons/io';
 import { RiDeleteBin6Line } from 'react-icons/ri';
+import { useState } from 'react';
 
 
-const EditNote = () => {
+const EditNote = ({notes, setNotes}) => {
+  const {id} = useParams();
+  const note = notes.find((itme) => itme.id === id);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.title);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if(title && details) {
+      const newNote = {...note, title, details}
+    }
+  }
   return (
     <section>
       <header className="create-note__header ">
@@ -12,8 +25,8 @@ const EditNote = () => {
       <button className='btn danger'><RiDeleteBin6Line/></button>
       </header>
       <form className='create-note__form'>
-        <input type="text" placeholder="Title" autoFocus/>
-        <textarea  rows='28' placeholder="Note details..."></textarea>
+        <input type="text" placeholder="Title" value={title} onchange={(e) => setTitle(e.target.value)} autoFocus/>
+        <textarea  rows='28' placeholder="Note details..." value={details} onchange={(e) => setDetails(e.target.value)}> </textarea>
       </form>
     </section>
   )
