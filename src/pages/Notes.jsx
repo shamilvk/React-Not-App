e24a5c6cb10d8c24a15d0a01afdef732736d4605
muhammesd shamil <107235517+shamilvk@users.noreply.github.com>
2023-02-01 00:23:29 +0530

    Create Note functionality

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,11 +1,10 @@
 import {CiSearch} from 'react-icons/ci'
-import dummyNotes from '../dummy_notes'
 import { Link } from 'react-router-dom'
 import {BsPlusLg} from 'react-icons/bs'
 
 import NoteItem from '../components/NoteItem'
 
-const Notes = () => {
+const Notes = ({notes}) => {
     return (
 
         <section>
@@ -16,7 +15,7 @@ const Notes = () => {
             </header>
             <div className="notes__container">
                {
-                dummyNotes.map(note => <NoteItem key={note.id} note={note} />)
+                notes.map(note => <NoteItem key={note.id} note={note} />)
                }
                <Link className='btn add__btn'><BsPlusLg/></Link>
             </div>
